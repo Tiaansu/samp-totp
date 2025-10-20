fe82f440c0012813f937e75d01040bebb8b3b124
Tiaansu <dev.marlon.cabading@gmail.com>
2025-10-20 13:25:53 +0800

    make the MAX_TOTP_SECRET_LENGTH 33 since the secret's length is 32 and add a warning in the console for smaller output size

--- src/natives.rs
+++ src/natives.rs
@@ -1,23 +1,51 @@
 use crate::internals::*;
+use log::error;
 use samp::error::AmxError;
 use samp::native;
 use samp::prelude::*;
 
 impl super::SampTotp {
     #[native(name = "totp_generate_secret")]
-    pub fn totp_generate_secret(&mut self, _: &Amx, dest: UnsizedBuffer, size: usize) -> AmxResult<bool> {
+    pub fn totp_generate_secret(
+        &mut self,
+        _: &Amx,
+        dest: UnsizedBuffer,
+        size: usize,
+    ) -> AmxResult<bool> {
         let secret = generate_setup_key();
 
         let mut dest = dest.into_sized_buffer(size);
-        let _ = samp::cell::string::put_in_buffer(&mut dest, secret.as_str());
+        let result = samp::cell::string::put_in_buffer(&mut dest, secret.as_str());
+
+        if result.is_err() {
+            match result.err().unwrap() {
+                AmxError::General => {
+                    error!(
+                        "The output buffer is too small. Expected: {} Provided: {}",
+                        secret.len(),
+                        size
+                    );
+                    return Ok(false);
+                }
+                _ => {
+                    return Ok(false);
+                }
+            }
+        }
 
         Ok(true)
     }
 
     #[native(raw, name = "totp_verify")]
     pub fn totp_verify(&mut self, _: &Amx, mut args: samp::args::Args) -> AmxResult<bool> {
-        let secret = args.next::<AmxString>().ok_or(AmxError::Params)?.to_string();
-        let otp = args.next::<AmxString>().ok_or(AmxError::Params)?.to_string();
+        let secret = args
+            .next::<AmxString>()
+            .ok_or(AmxError::Params)?
+            .to_string();
+        let otp = args
+            .next::<AmxString>()
+            .ok_or(AmxError::Params)?
+            .to_string();
 
         let totp = create_totp_from_key(&secret);
         if totp.is_err() {
